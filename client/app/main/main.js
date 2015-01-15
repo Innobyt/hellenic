1b398a1449783b384d870868de555c19dfd43701
Calvyn Tng <calvyn.twh@gmail.com>
2015-01-15 13:20:11 +0800

    init

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('hellenicApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
