a18c7ad00b664ee741a2b48fcbaade7eb9cdb9a8
Kyle Haithir <kyle@innobyt.com>
2015-01-16 11:38:54 +0800

    add disbursement maintenance route

---
+++ server/api/disbursement_maintenance/disbursement_maintenance.controller.js
@@ -0,0 +1,52 @@
+'use strict';
+
+var disbursement_maintenance = require('./disbursement_maintenance.model');
+
+/**
+ * 
+ */
+exports.index = function(req, res){
+
+}
+
+/**
+ * 
+ */
+exports.show = function(req, res){
+
+}
+
+/**
+ * 
+ */
+exports.create = function(req, res){
+
+}
+
+/**
+ * 
+ */
+exports.update = function(req, res){
+
+}
+
+/**
+ * 
+ */
+exports.update = function(req, res){
+
+}
+
+/**
+ * 
+ */
+exports.destroy = function(req, res){
+
+}
+
+/**
+ * 
+ */
+function handleError(res, err) {
+  return res.send(500, err);
+ };
---
+++ server/api/disbursement_maintenance/disbursement_maintenance.model.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var disbursement_maintenance = new Schema({
+
+});
+
+module.exports = mongoose.model('disbursement_maintenance', disbursement_maintenance);
