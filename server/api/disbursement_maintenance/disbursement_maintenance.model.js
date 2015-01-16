7aa54c12af1f7d283b1ecfde6a9d9760f9eb99be
Kyle Haithir <kyle@innobyt.com>
2015-01-16 14:22:33 +0800

    add schema disbursement maintenance

--- server/api/disbursement_maintenance/disbursement_maintenance.model.js
+++ server/api/disbursement_maintenance/disbursement_maintenance.model.js
@@ -4,7 +4,28 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var disbursement_maintenance = new Schema({
-
+    created         : String,
+    user            : String,
+    arvd            : String,
+    t_date          : String,
+    cd_type         : String,
+    invoice_no      : String,
+    vendor_acno     : String,
+    invoice_date    : String,
+    expense_accode  : String,
+    desc            : String,
+    cost            : String,
+    billing         : String,
+    gst             : String,
+    gst_percentage  : String,
+    remarks         : String,
+    vessel_name     : String,
+    vessel_number   : String,
+    customer_number : String,
+    customer_name   : String,
+    drcr_number     : String,
+    hide            : String,
+    approve        	: String
 });
 
 module.exports = mongoose.model('disbursement_maintenance', disbursement_maintenance);
