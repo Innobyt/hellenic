671ce48ee8eed4e4733967979d3806626a9c280d
Kyle Haithir <kyle@innobyt.com>
2015-01-19 16:03:07 +0800

    add customer.model.js

---
+++ server/api/customer/customer.model.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+/**
+ * customer Schema
+ */
+var customer  = new Schema({
+    created         : String,
+    user            : String,
+    number    		: String,
+    name      		: String,
+    hide            : String,
+    approval        : String
+});
+
+module.exports = mongoose.model('customer ', customer );
