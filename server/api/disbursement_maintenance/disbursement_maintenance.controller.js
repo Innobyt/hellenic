3434ebd4093c0e5606fb2d13b455b77bb0a81dec
Kyle Haithir <kyle@innobyt.com>
2015-01-19 11:22:05 +0800

    add update, http 400 error - disbursement maintenance fix http_error_notfound http code from 401 to 404 add http_error_notfound to show function

--- server/api/disbursement_maintenance/disbursement_maintenance.controller.js
+++ server/api/disbursement_maintenance/disbursement_maintenance.controller.js
@@ -17,12 +17,11 @@ exports.index = function(req, res){
  * Get disbursement maintenance entry
  */
 exports.show = function(req, res){
-
-	var disbursement_maintenance_id = req.params.id;
-    disbursement_maintenance.findById(disbursement_maintenance_id, function(err, disbursement_maintenance_document){
+	var queryid = req.params.id;
+    disbursement_maintenance.findById(queryid, function(err, doc){
         if (err) return handleError(res, err);
-        if (!user) return http_error_notfound(res, err);
-        res.json(disbursement_maintenance_document);
+        if (!doc) return http_error_notfound(res, err);
+        res.json(doc);
     });
 }
 
@@ -39,17 +38,29 @@ exports.create = function(req, res){
 }
 
 /**
- * 
+ * Update disbursement maintenance document 
  */
 exports.update = function(req, res){
+	
+	// get document disbursement maintenance document id
+	var id = req.params.id;
 
+	// get document disbursement maintenance document id update fields
+	var update = req.body;
+
+	// update disbursement maintenance collection document of id query, with update properties of update
+	disbursement_maintenance.Model.findByIdAndUpdate(id, update, function(err, numberAffected, raw){
+		if(err) return handleError(res, err);
+		if(!numberAffected) return http_error_badrequest(res, err);
+		res.json(200);
+	});
 }
 
 /**
  * Deletes disbursement maintenance entry
  */
 exports.destroy = function(req, res){
-  disbursement_maintenance.findByIdAndRemove(req.params.id, function(err, destroyed_disbursement_maintenance) {
+  disbursement_maintenance.findByIdAndRemove(req.params.id, function(err, doc) {
 	return err ? handleError(res, err) : res.json(204); // 204 No Content - response to a successful delete request
   });
 }
@@ -76,5 +87,14 @@ function validationError(res, err) {
  * future. Subsequent requests by the client are permissible.
  */
 function http_error_notfound(res, err) {
-  return res.json(401, err);
+  return res.json(404, err);
+};
+
+/**
+ * 400 Bad Request
+ * The server cannot or will not process the request due to something that is p
+ * erceived to be a client error.
+ */
+function http_error_badrequest(res, err) {
+  return res.json(400, err);
 };
