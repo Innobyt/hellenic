364185a412e4d35158f60f4972c3a745527cfaa2
Kyle Haithir <kyle@innobyt.com>
2015-01-19 10:57:17 +0800

    add http 500 description

--- server/api/disbursement_maintenance/disbursement_maintenance.controller.js
+++ server/api/disbursement_maintenance/disbursement_maintenance.controller.js
@@ -56,6 +56,8 @@ exports.destroy = function(req, res){
 
 /**
  * 500 Internal Server Error
+ * A generic error message, given when an unexpected condition was encountered 
+ * and no more specific message is suitable.
  */
 function handleError(res, err) {
   return res.send(500, err);
