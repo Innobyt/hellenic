ad1fbb51ba4178947aa5b4d78797e1dde46c9da5
Kyle Haithir <kyle@innobyt.com>
2015-01-22 16:13:12 +0800

    change vesseltable stateProvider state names

--- client/app/vesseltable/vesseltable.js
+++ client/app/vesseltable/vesseltable.js
@@ -3,25 +3,25 @@
 angular.module('hellenicApp')
   .config(function ($stateProvider) {
     $stateProvider
-	.state('create', {
+	.state('create_vesseltable', {
         url: '/vesseltable/create',
         templateUrl: 'app/vesseltable/create/create.html',
         controller: 'CreateCtrl',
         authenticate: true
       })
-    .state('list', {
+    .state('list_vesseltable', {
         url: '/vesseltable/list',
         templateUrl: 'app/vesseltable/list/list.html',
         controller: 'ListCtrl',
         authenticate: true
       })
-    .state('query', {
+    .state('query_vesseltable', {
         url: '/vesseltable/query/:id',
         templateUrl: 'app/vesseltable/query/query.html',
         controller: 'QueryCtrl',
         authenticate: true
       })
-      .state('update', {
+      .state('update_vesseltable', {
         url: '/vesseltable/update/:id',
         templateUrl: 'app/vesseltable/update/update.html',
         controller: 'UpdateCtrl',
