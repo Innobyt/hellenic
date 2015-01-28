f429779d717e4abc0236fde2eafdd5c51e5e419c
Kyle Haithir <kyle@innobyt.com>
2015-01-28 10:38:01 +0800

    change generate credit debit note schema

--- server/api/generatecreditdebitnote/generatecreditdebitnote.model.js
+++ server/api/generatecreditdebitnote/generatecreditdebitnote.model.js
@@ -7,15 +7,15 @@ var mongoose = require('mongoose'),
  * generatecreditdebitnote Schema
  */
 var generatecreditdebitnote = new Schema({
-    created		: String, 
-    user		: String, 
-    vessel_no	: String, 
-    customer_no	: String, 
-    arvd		: String, 
-    print_date	: String, 
-    cdnote_no	: String, 
-    hide		: String,
-    approval	: String
+    created             : String, 
+    user                : String, 
+    vessel_number       : String, 
+    customer_number     : String, 
+    arvd                : String, 
+    print_date          : String, 
+    cdnote_no           : String, 
+    hide                : String,
+    approval            : String
 });
 
 module.exports = mongoose.model('generatecreditdebitnote', generatecreditdebitnote);
