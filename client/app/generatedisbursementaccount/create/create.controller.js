15208054e743bded9ed42e8ad14bb16f9866043c
Kyle Haithir <kyle@innobyt.com>
2015-02-06 17:13:51 +0800

    change form input to drop down selection, vesselnumber and customer number

--- client/app/generatedisbursementaccount/create/create.controller.js
+++ client/app/generatedisbursementaccount/create/create.controller.js
@@ -4,14 +4,24 @@
 	angular.module('hellenicApp')
 	  .controller('generatedisbursementaccountCreateCtrl', generatedisbursementaccountCreateCtrl);
 	
-	generatedisbursementaccountCreateCtrl.$inject=['$scope', 'generatedisbursementaccount'];
+	generatedisbursementaccountCreateCtrl.$inject=['$scope', 'generatedisbursementaccount', 'vesseltable', 'customertable'];
 
-	function generatedisbursementaccountCreateCtrl($scope, generatedisbursementaccount) {
+	function generatedisbursementaccountCreateCtrl($scope, generatedisbursementaccount, vesseltable, customertable) {
 
 		// initialize create controller
 		$scope.initialize = function(){
 			// create new instance of generatedisbursementaccount
 			$scope.formData = new generatedisbursementaccount();
+			// get vesseltable collection from the factory
+			vesseltable.view().$promise.then(
+				function(response) {
+					$scope.vessels = response;
+				});
+			// get customertable collection from the factory
+			customertable.view().$promise.then(
+				function(response) {
+					$scope.customers = response;
+				});
 		};
 
 		// submit customer table form
