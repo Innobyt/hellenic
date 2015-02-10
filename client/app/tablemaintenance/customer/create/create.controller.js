3dd39558dcd6826988c87f7904de68fbb34b9957
Kyle Haithir <kyle@innobyt.com>
2015-02-10 17:47:29 +0800

    add get vesseltable documents - table maintenance customer controller add get customertable documents - table maintenance customer controller

--- client/app/tablemaintenance/customer/create/create.controller.js
+++ client/app/tablemaintenance/customer/create/create.controller.js
@@ -4,14 +4,24 @@
 	angular.module('hellenicApp')
 	  .controller('tablemaintenance_customerCreateCtrl', tablemaintenance_customerCreateCtrl);
 	
-	tablemaintenance_customerCreateCtrl.$inject=['$scope', 'tablemaintenance_customer'];
+	tablemaintenance_customerCreateCtrl.$inject=['$scope', 'tablemaintenance_customer', 'vesseltable', 'customertable'];
 
-	function tablemaintenance_customerCreateCtrl($scope, tablemaintenance_customer) {
+	function tablemaintenance_customerCreateCtrl($scope, tablemaintenance_customer, vesseltable, customertable) {
 
 		// initialize create controller
 		$scope.initialize = function(){
 			// create new instance of tablemaintenance_customer
 			$scope.formData = new tablemaintenance_customer();
+			// get vesseltable collection from the factory
+			vesseltable.view().$promise.then(
+				function(response) {
+					$scope.vessels = response;
+				});
+			// get customertable collection from the factory
+			customertable.view().$promise.then(
+				function(response) {
+					$scope.customers = response;
+				});
 		};
 
 		// submit customer table form
