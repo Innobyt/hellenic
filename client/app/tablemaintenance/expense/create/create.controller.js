21b611a9204a8e5508fcf87a84b792d06c56d406
Kyle Haithir <kyle@innobyt.com>
2015-02-12 11:48:24 +0800

    add front-end form fields - table maintenance expense

--- client/app/tablemaintenance/expense/create/create.controller.js
+++ client/app/tablemaintenance/expense/create/create.controller.js
@@ -3,15 +3,20 @@
 
 	angular.module('hellenicApp')
 	  .controller('tablemaintenance_expenseCreateCtrl', tablemaintenance_expenseCreateCtrl);
-	
-	tablemaintenance_expenseCreateCtrl.$inject=['$scope', 'tablemaintenance_expense'];
 
-	function tablemaintenance_expenseCreateCtrl($scope, tablemaintenance_expense) {
+	tablemaintenance_expenseCreateCtrl.$inject=['$scope', 'tablemaintenance_expense', 'expensetable'];
+
+	function tablemaintenance_expenseCreateCtrl($scope, tablemaintenance_expense, expensetable) {
 
 		// initialize create controller
 		$scope.initialize = function(){
 			// create new instance of tablemaintenance_expense
 			$scope.formData = new tablemaintenance_expense();
+			// get expensetable collection from the factory
+			expensetable.view().$promise.then(
+				function(response) {
+					$scope.expenses = response;
+				});
 		};
 
 		// submit expense table form
--- client/app/tablemaintenance/expense/update/update.controller.js
+++ client/app/tablemaintenance/expense/update/update.controller.js
@@ -4,9 +4,9 @@
 	angular.module('hellenicApp')
 	  .controller('tablemaintenance_expenseUpdateCtrl', tablemaintenance_expenseUpdateCtrl);
 	
-	tablemaintenance_expenseUpdateCtrl.$inject=['$scope', '$stateParams', 'tablemaintenance_expense'];
+	tablemaintenance_expenseUpdateCtrl.$inject=['$scope', '$stateParams', 'tablemaintenance_expense', 'expensetable'];
 
-	function tablemaintenance_expenseUpdateCtrl($scope, $stateParams, tablemaintenance_expense) {
+	function tablemaintenance_expenseUpdateCtrl($scope, $stateParams, tablemaintenance_expense, expensetable) {
 
 		// get a tablemaintenance_expense object from the factory
 		$scope.tablemaintenance_expense = tablemaintenance_expense.query({ id: $stateParams.id });
@@ -15,6 +15,11 @@
 		$scope.initialize = function(){
 			// create new instance of tablemaintenance_expense
 			$scope.formData = new tablemaintenance_expense();
+			// get expensetable collection from the factory
+			expensetable.view().$promise.then(
+				function(response) {
+					$scope.expenses = response;
+				});
 		};
 
 		$scope.submit = function() {
