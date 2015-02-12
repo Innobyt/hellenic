cadbd4dd22066b74820b1b5505568c4be0c20bd1
Kyle Haithir <kyle@innobyt.com>
2015-02-12 14:34:02 +0800

    add get customertable collection - billing maintenance debit, update change form field type from text to select - billing maintenance debit, update

--- client/app/billingmaintenancedebit/update/update.controller.js
+++ client/app/billingmaintenancedebit/update/update.controller.js
@@ -4,9 +4,9 @@
 	angular.module('hellenicApp')
 	  .controller('billingmaintenancedebitUpdateCtrl', billingmaintenancedebitUpdateCtrl);
 	
-	billingmaintenancedebitUpdateCtrl.$inject=['$scope', '$stateParams', 'billingmaintenancedebit'];
+	billingmaintenancedebitUpdateCtrl.$inject=['$scope', '$stateParams', 'billingmaintenancedebit', 'vesseltable' , 'customertable'];
 
-	function billingmaintenancedebitUpdateCtrl($scope, $stateParams, billingmaintenancedebit) {
+	function billingmaintenancedebitUpdateCtrl($scope, $stateParams, billingmaintenancedebit, vesseltable, customertable) {
 
 		// get a billingmaintenancedebit object from the factory
 		$scope.billingmaintenancedebit = billingmaintenancedebit.query({ id: $stateParams.id });
@@ -15,6 +15,16 @@
 		$scope.initialize = function(){
 			// create new instance of billingmaintenancedebit
 			$scope.formData = new billingmaintenancedebit();
+			// get vesseltable collection from the factory
+			vesseltable.view().$promise.then(
+				function(response) {
+					$scope.vessels = response;
+				});
+			// get customertable collection from the factory
+			customertable.view().$promise.then(
+				function(response) {
+					$scope.customers = response;
+				});
 		};
 
 		$scope.submit = function() {
