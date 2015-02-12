490d9ecdc338cb41cd4c89f1332c715eb27e7893
Kyle Haithir <kyle@innobyt.com>
2015-02-12 14:02:50 +0800

    add get vesseltable collection - billing maintenance debit change form field type from text to select - billing maintenance debit

--- client/app/billingmaintenancedebit/create/create.controller.js
+++ client/app/billingmaintenancedebit/create/create.controller.js
@@ -4,14 +4,19 @@
 	angular.module('hellenicApp')
 	  .controller('billingmaintenancedebitCreateCtrl', billingmaintenancedebitCreateCtrl);
 	
-	billingmaintenancedebitCreateCtrl.$inject=['$scope', 'billingmaintenancedebit'];
+	billingmaintenancedebitCreateCtrl.$inject=['$scope', 'billingmaintenancedebit', 'vesseltable'];
 
-	function billingmaintenancedebitCreateCtrl($scope, billingmaintenancedebit) {
+	function billingmaintenancedebitCreateCtrl($scope, billingmaintenancedebit, vesseltable) {
 
 		// initialize create controller
 		$scope.initialize = function(){
 			// create new instance of billingmaintenancedebit
 			$scope.formData = new billingmaintenancedebit();
+			// get vesseltable collection from the factory
+			vesseltable.view().$promise.then(
+				function(response) {
+					$scope.vessels = response;
+				});
 		};
 
 		// submit customer table form
