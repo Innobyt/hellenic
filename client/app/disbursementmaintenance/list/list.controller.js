ed7e7c89f9ea729859781388b7217b407ddde95d
Calvyn Tng <calvyn.twh@gmail.com>
2015-02-26 15:23:27 +0800

    refactor ngtable to uigrid for disbursementmaintenance

--- client/app/disbursementmaintenance/list/list.controller.js
+++ client/app/disbursementmaintenance/list/list.controller.js
@@ -12,31 +12,63 @@
 		// get a disbursementmaintenance collection from the factory
 		var data = $scope.list = disbursementmaintenance.view();
 
-		$scope.tableParams = new ngTableParams({
-			// show first page
-			page: 1,            
-			// count per page
-			count: 10,          
-			// initial filter
-			filter: { number: '' },
-			// initial sorting
-			sorting: { number: 'asc' }
-		}, {
-			// length of data
-			total: data.length,
-			getData: function($defer, params) {
-			// use build-in angular filter
-				var filteredData = params.filter() ?
-					$filter('filter')(data, params.filter()) :
-					data;
-				var orderedData = params.sorting() ?
-					$filter('orderBy')(filteredData, params.orderBy()) :
-					data;
+		/*** Export options in http://ui-grid.info/docs/#/tutorial/206_exporting_data */
+		/*** gridOptions in https://github.com/angular-ui/ui-grid/wiki/Configuration-Options */
+		$scope.gridOptions = {
+    		enableGridMenu: true,
+		    exporterPdfDefaultStyle: {fontSize: 9},
+    		exporterPdfTableHeaderStyle: {fontSize: 10, bold: false},
+		    exporterPdfOrientation: 'landscape',
+			data: data
+		};
 
-				params.total(orderedData.length); // set total for recalc pagination
-				$defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
-			}
+		data.$promise.then(function(rs){
+			$scope.gridOptions.columnDefs = generateHeaders(rs);
 		});
+
+		function generateHeaders(data){
+			var rs = [],
+				sample = data[0];
+
+			for (data in sample){
+				if (typeof sample[data] === 'function' || data[0] === '_'){
+					continue;
+				}
+				/*** Config options in https://github.com/angular-ui/ui-grid/wiki/Defining-columns */
+				rs.push({
+					name: data, 
+					width:'150',
+				});
+			}
+			return rs;
+		}
+
+		/*** ng-table codes */
+		// $scope.tableParams = new ngTableParams({
+		// 	// show first page
+		// 	page: 1,            
+		// 	// count per page
+		// 	count: 10,          
+		// 	// initial filter
+		// 	filter: { number: '' },
+		// 	// initial sorting
+		// 	sorting: { number: 'asc' }
+		// }, {
+		// 	// length of data
+		// 	total: data.length,
+		// 	getData: function($defer, params) {
+		// 	// use build-in angular filter
+		// 		var filteredData = params.filter() ?
+		// 			$filter('filter')(data, params.filter()) :
+		// 			data;
+		// 		var orderedData = params.sorting() ?
+		// 			$filter('orderBy')(filteredData, params.orderBy()) :
+		// 			data;
+
+		// 		params.total(orderedData.length); // set total for recalc pagination
+		// 		$defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+		// 	}
+		// });
 
 	}
 })();
